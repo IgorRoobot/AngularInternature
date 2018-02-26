445c7ca0ace291d43f663cb849dbe4fe54da64ab
Andru Ischenko <roobotik@gmail.com>
2018-02-26 13:55:13 +0200

    test array

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -13,6 +13,7 @@ export class CartComponent implements OnInit {
   toggle = true;
 
   saveCarts = '';
+  saveCartsArray = [];
   carts: CartModel[] = [
     {id: 1, name: 'Name 1', status: false},
     {id: 2, name: 'Name 2', status: false},
@@ -25,46 +26,15 @@ export class CartComponent implements OnInit {
   }
 
   checkAll() {
-    // let t = [];
-    // this.carts.map( i => t.push(i));
-
     this.carts.map( elem => elem.status = this.flag);
     this.flag = !this.flag;
-
-//    this.saveCarts = t;
   }
 
   loadData() {
   if (this.toggle) {
-this.carts = JSON.parse(this.saveCarts);
-      /*
-        () =>  ....
-        (x) =>  x+1
-        (x) => { return console.log(x+1); }
-      */
-    //  for(let i=0; i < this.carts.length; i++) {
-    //   for(let x=0; x< this.saveCarts.length; x++) {
-    //    let temp = this.saveCarts[x].split('_');
-
-    //     if (this.carts[i].id == temp[0]) {
-    //       this.carts[i].status = temp[1];
-    //     } else {
-    //       this.carts[i].status = false;
-    //     }
-    //   }
-    //  }
-
-      // this.carts.map(i => {
-      //   if (i.id == this.saveCarts[ this.saveCarts.length-1 ].id) {
-      //     return i.status =  !this.saveCarts[ this.saveCarts.length-1 ].status;
-      //   }
-      //   else {
-      //     return i.status = !i.status;
-      //   }
-      // });
+      this.carts = JSON.parse(this.saveCarts);
       console.log('load data');
   }
-
     this.toggle = !this.toggle;
     // console.log(this.saveCarts);
     // this.carts = [];
@@ -81,8 +51,8 @@ this.carts = JSON.parse(this.saveCarts);
         }
     });
  
-     this.saveCarts = JSON.stringify(this.carts).toString();
-    // Array.prototype.push.apply(this.saveCarts, this.carts);
-  //   console.log('saved',this.saveCarts);
+    this.saveCarts = JSON.stringify(this.carts).toString();
+    this.saveCartsArray = this.carts;
+    console.log (this.carts);
   } 
 }
